87863b7e4074d028dd8213c66c5bd3b3970a41b2
PHOYSAL <phoysal0007@gmail.com>
2025-08-27 12:47:14 +0600

    call and call coin functionality added

--- script/custom.js
+++ script/custom.js
@@ -3,6 +3,7 @@ function customElement(id) {
 }
 
 
+
 document.getElementById("card-container").addEventListener("click", function (event) {
     
     if (event.target.className.includes("heart-icon")) {
@@ -12,4 +13,25 @@ document.getElementById("card-container").addEventListener("click", function (ev
         customElement("heart-point").innerText = updateHeartNumber;
     }
 
+})
+document.getElementById("card-container").addEventListener("click",function(event){
+    // console.log(event.target.className.includes("call-btn"))
+    if(event.target.className.includes("call-btn")){
+        const btn=event.target;
+        // console.log(btn)
+        const title=btn.parentNode.parentNode.children[1].children[0].innerText;
+        const callNumber=btn.parentNode.parentNode.children[1].children[2].innerText
+        // console.log(callNumber)
+        // alert(`${title} and ${callNumber}`)
+        // console.log(customElement("call-coin").innerText)
+        const callCoin=Number(customElement("call-coin").innerText)
+        if(callCoin>0){
+            alert(`${title} and ${callNumber}`)
+            const afterCallCoin=callCoin-20;
+            customElement("call-coin").innerText=afterCallCoin;
+        }
+        else{
+            alert("insufficient balance");
+        }
+    }
 })
