dc3a732460f308245ea9cc0c1ff0eef315504ed9
PHOYSAL <phoysal0007@gmail.com>
2025-08-27 14:57:58 +0600

    history and clear btn functionality done

--- script/custom.js
+++ script/custom.js
@@ -25,13 +25,48 @@ document.getElementById("card-container").addEventListener("click",function(even
         // alert(`${title} and ${callNumber}`)
         // console.log(customElement("call-coin").innerText)
         const callCoin=Number(customElement("call-coin").innerText)
+        const historyContainer=customElement("history-container");
+        // console.log(historyContainer)
+        const newDiv=document.createElement("div")
+        // newDiv.innerHTML=`
+        // <div class="bg-[#D4D6D5] p-5 rounded-lg flex justify-between items-center mb-2">
+        //                         <div>
+        //                             <h2 class="font-bold">${title}</h2>
+        //                             <p class="text-gray-500">${callNumber}</p>
+        //                         </div>
+        //                         <div>
+        //                             <span class="text-gray-500">12:19:32 AM</span>
+        //                         </div>
+        //                     </div>
+        // `
+        // historyContainer.appendChild(newDiv)
+
+        
         if(callCoin>0){
-            alert(`${title} and ${callNumber}`)
+            alert(`Calling ${title} ${callNumber}...`)
             const afterCallCoin=callCoin-20;
             customElement("call-coin").innerText=afterCallCoin;
+
+            newDiv.innerHTML=`
+        <div class="bg-[#D4D6D5] p-5 rounded-lg flex justify-between items-center mb-2">
+                                <div>
+                                    <h2 class="font-bold">${title}</h2>
+                                    <p class="text-gray-500">${callNumber}</p>
+                                </div>
+                                <div>
+                                    <span class="text-gray-500">2:49 PM</span>
+                                </div>
+                            </div>
+        `
+        historyContainer.appendChild(newDiv)
         }
         else{
-            alert("insufficient balance");
+            alert("You have no sufficient coins. Need 20 coins or more...");
+            
         }
     }
+    customElement("clear-btn").addEventListener("click",function(){
+        customElement("history-container").innerText=""
+    });
+    
 })
