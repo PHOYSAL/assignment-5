6ea9facf54d82ade080cc2190f77ffab0093def5
PHOYSAL <phoysal0007@gmail.com>
2025-08-27 10:35:03 +0600

    heart icon click and update point

--- script/custom.js
+++ script/custom.js
@@ -1,1 +1,15 @@
-console.log("clicked")
+function customElement(id) {
+    return (document.getElementById(id));
+}
+
+
+document.getElementById("card-container").addEventListener("click", function (event) {
+    
+    if (event.target.className.includes("heart-icon")) {
+       
+        const heartPointNumber = Number(customElement("heart-point").innerText)
+        let updateHeartNumber = heartPointNumber + 1;
+        customElement("heart-point").innerText = updateHeartNumber;
+    }
+
+})
